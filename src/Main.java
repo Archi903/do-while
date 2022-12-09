f319614a51ed6489f5551bfe190b414f0a1c65f8
Archi903 <artpele@yandex.ru>
2022-12-09 12:47:31 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
+    public static void task1() {
+        System.out.println("Задание 1");
+        int money = 29000;
+        int total = 0;
+        int i = 1;
+        while (total < 2_459_000) {
+            total = total + total / 100;
+            total = total + money;
+            i = i + 1;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+        }
     }
 }
