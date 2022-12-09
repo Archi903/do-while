053f95c7d9e462b0cabfcc8fc3daea871d2df240
Archi903 <artpele@yandex.ru>
2022-12-09 15:37:27 +0300

    Задание 4

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1() {
         System.out.println("Задание 1");
-        int money = 29000;
+        int money = 15000;
         int total = 0;
         int i = 1;
         while (total < 2_459_000) {
@@ -43,8 +44,8 @@ public class Main {
         int birth = oneHuman * 18;
         int death = oneHuman * 8;
         int totalInYear = birth - death;
-        int addPeople =0;
-        for(int year = 1; year <= 10; year ++) {
+        int addPeople = 0;
+        for (int year = 1; year <= 10; year++) {
             Y = Y + totalInYear + addPeople;
 
             System.out.println("Год " + year + " численность населения составляет " + Y);
@@ -52,4 +53,16 @@ public class Main {
 
 
     }
+
+    public static void task4() {
+        System.out.println("Задание 4");
+
+        int deposit = 15_000;
+        for (int i = 1; deposit <= 12_000_000; i++) {
+            deposit = deposit / 100 * 7 + (deposit);
+            System.out.println("Месяц " + i + " сумма накоплений равна " + deposit + " рублей");
+        }
+    }
 }
+
+
